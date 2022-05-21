1dca400e5b2a06de5ff2993ec756ac87cf5f328a
reza <reza9300@gmail.com>
2022-05-21 20:17:00 +0430

    Initial commit

---
+++ common/src/androidMain/kotlin/com/rezarashidi/common/platform.kt
@@ -0,0 +1,5 @@
+package com.rezarashidi.common
+
+actual fun getPlatformName(): String {
+    return "Android"
+}
---
+++ common/src/commonMain/kotlin/com/rezarashidi/common/platform.kt
@@ -0,0 +1,3 @@
+package com.rezarashidi.common
+
+expect fun getPlatformName(): String
